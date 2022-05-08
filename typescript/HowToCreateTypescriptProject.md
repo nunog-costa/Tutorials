0962620f8a62bafe759d97d52508c5fdc2881ad7
Nuno Costa <work.nunoc@gmail.com>
2022-05-08 21:56:47 +0100

    Added functionOverload example

---
+++ typescript/HowToCreateTypescriptProject.md
@@ -0,0 +1,11 @@
+# Tutorial: https://www.freecodecamp.org/news/how-to-set-up-a-typescript-project-67b427114884/
+
+## How to install TypeScript compiler:
+
+npm install --global typescript
+
+## if you would like to run a TypeScript file directly from the command prompt, the easiest way is to use ts-node:
+
+npm install --global ts-node
+
+## After installing ts-node, run ts-node X.ts where X.ts is the name of a script you want to run.
---
+++ typescript/functionOverload.ts
@@ -0,0 +1,49 @@
+/*
+ *  Tutorial: No BS TS #4 - Function Overloading in Typescript
+ *  URL: https://www.youtube.com/watch?v=XnyZXNnWAOA
+ */
+
+// Challenge: overload a function that can take 3 types of arguments
+
+// Let's create an interface that will be used as arg
+interface Coordinate {
+  x: number;
+  y: number;
+}
+
+// Create the overloaded functions without an implementation
+function parseCoordinate(str: string): Coordinate;
+function parseCoordinate(obj: Coordinate): Coordinate;
+function parseCoordinate(x: number, y: number): Coordinate;
+
+// Now lets create the actual implementation
+// Note: 'unknown' type works like 'any' except that when used, TypeScript enforces a type check so we need to use casting.
+function parseCoordinate(arg1: unknown, arg2?: unknown): Coordinate {
+  let coord: Coordinate = {
+    x: 0,
+    y: 0,
+  };
+
+  // This type check occurs during runtime
+  if (typeof arg1 === 'string') {
+    (arg1 as string).split(',').forEach((str) => {
+      const [key, value] = str.split(':');
+      coord[key as 'x' | 'y'] = parseInt(value, 10);
+    });
+  } else if (typeof arg1 === 'object') {
+    coord = {
+      ...(arg1 as Coordinate),
+    };
+  } else {
+    coord = {
+      x: arg1 as number,
+      y: arg2 as number,
+    };
+  }
+
+  return coord;
+}
+
+console.log(parseCoordinate('x:1,y:2'));
+console.log(parseCoordinate({ x: 1, y: 2 }));
+console.log(parseCoordinate(1, 2));
